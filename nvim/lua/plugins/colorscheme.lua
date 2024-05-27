2db3da1c8884bef1bf643aea4b882cc55ce371a3
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-05-27 08:12:58 -0300

    feat: track nvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "LazyVim/LazyVim", opts = {
+  --   colorscheme = "gruvbox",
+  -- } },
+  { "catppuccin/nvim", name = "catppuccin", lazy = true, priority = 1000 },
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
+  -- {
+  --   "craftzdog/solarized-osaka.nvim",
+  --   lazy = true,
+  --   priority = 1000,
+  -- },
+}
