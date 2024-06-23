a8810285af6cf4fa77ba3ab82f469af6162ddae7
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-06-23 09:56:38 -0300

    feat: add prisma support to neovim

---
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "eslint@4.8.0", "prismals" },
+      })
+    end,
+  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     local lspconfig = require("lspconfig")
+  --     lspconfig.prismals.setup({})
+  --   end,
+  -- },
+}
