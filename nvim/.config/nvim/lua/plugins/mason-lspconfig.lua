02321aa0404cf669b6ed1f241d515b45fbb7fc31
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-07-26 07:22:37 -0300

    bugfix: fix issues with mason and dashboard in my new install

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,7 @@
 return {
   {
     "williamboman/mason-lspconfig.nvim",
+    enabled = true,
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "eslint@4.8.0", "prismals" },
