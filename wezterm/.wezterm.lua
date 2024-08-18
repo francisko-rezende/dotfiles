98766d0a5f1166805187fc3c8d6606fbb3950386
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-08-18 17:06:38 -0300

    feat: remove wezterm tab

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -12,5 +12,11 @@ config.color_scheme = "Catppuccin Mocha"
 -- Font
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 
+-- Tab bar
+config.enable_tab_bar = false
+
+-- 
+config.window_decorations = "RESIZE"
+
 -- and finally, return the configuration to wezterm
 return config
